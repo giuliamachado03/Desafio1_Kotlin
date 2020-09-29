8062bf6d6e806de7f6a75c763c78ca11ecc34a1e
Giulia Machado <giuliamachado03@gmail.com>
2020-09-29 16:37:48 -0300

    Classe curso - mudança construtor

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,30 +1,30 @@
  class Curso (var nome: String, var codigoDoCurso: Int, var qtdMaxAlunos: Int ) {
 
-    var professorTitular = arrayListOf<ProfessorTitular>()
-    var professorAdjunto = arrayListOf<ProfessorAdjunto>()
-    var listaAlunosMatriculados = arrayListOf<Aluno>()
+     var professorTitular = arrayListOf<ProfessorTitular>()
+     var professorAdjunto = arrayListOf<ProfessorAdjunto>()
+     var listaAlunosMatriculados = arrayListOf<Aluno>()
 
-    fun adicionarUmAluno(umAluno: Aluno): Any {
+     fun adicionarUmAluno(umAluno: Aluno): Any {
 
-        return if (listaAlunosMatriculados.contains(umAluno)) {
-            return false
-        } else if (listaAlunosMatriculados.size <= qtdMaxAlunos) {
-            listaAlunosMatriculados.add(umAluno)
-            return true
-        } else {
-        }
-    }
+         return if (listaAlunosMatriculados.contains(umAluno)) {
+             return false
+         } else if (listaAlunosMatriculados.size <= qtdMaxAlunos) {
+             listaAlunosMatriculados.add(umAluno)
+             return true
+         } else {
+         }
+     }
 
-    fun excluirAluno(umAluno: Aluno): Boolean {
+     fun excluirAluno(umAluno: Aluno): Boolean {
 
-        if (listaAlunosMatriculados.contains(umAluno)) {
-            listaAlunosMatriculados.remove(umAluno)
-            return true
-        } else {
-            return false
-        }
-    }
-}
+         if (listaAlunosMatriculados.contains(umAluno)) {
+             listaAlunosMatriculados.remove(umAluno)
+             return true
+         } else {
+             return false
+         }
+     }
+ }
 
 
 
